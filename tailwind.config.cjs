a79918338efb02a40cc4c1c4487822623a6ed26e
gevorah <jhonsebastianijajiortiz@gmail.com>
2023-07-13 13:49:43 -0500

    fix: theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#e60012',
+        'blue-dark': '#0b2b6b',
+        'blue-light': '#d6f0fa',
+        'white-soft': '#efefef',
+        'black-soft': '#434242',
+      },
+    },
+  },
+  plugins: [],
 }
